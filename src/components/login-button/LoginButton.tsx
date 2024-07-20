e1586bd9ee72d61838962b905643d5bebe431361
Avinash <ghale.avinash@gmail.com>
2024-07-20 12:20:06 +0900

    add loading skeleton for login button

--- src/components/login-button/LoginButton.tsx
+++ src/components/login-button/LoginButton.tsx
@@ -20,86 +20,99 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/src/components/ui/dropdown-menu"
+import { Skeleton } from "../skeleton"
+
+const LoginButtonLoadingSkeleton = () => {
+  return (
+    <Skeleton>
+      <div className="flex">
+        <div className="h-5 w-5 sm:mr-2 rounded-full bg-slate-200" />
+        <div className="hidden sm:inline w-20 rounded bg-slate-200" />
+      </div>
+    </Skeleton>
+  )
+}
 
 export const LoginButton = () => {
-  const { data: session, status } = useSession()
-  const { setTheme } = useTheme()
+  const { data: session, status: sessionStatus } = useSession()
   const { t } = useTranslation("common")
   const router = useRouter()
 
-  if (session) {
-    const { mobile, name, role } = session.user
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="sm">
-            <User className="h-5 w-5 sm:mr-2" />
-            <span className="hidden sm:inline">{name}</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="w-56" forceMount>
-          <DropdownMenuLabel>
-            <div>My Account</div>
-            <span className="text-xs font-normal text-slate-500">{mobile}</span>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem
-            className="cursor-pointer"
-            onClick={() => {
-              router.push("/user/tickets")
-            }}
-          >
-            <Contact className="mr-2 h-4 w-4" />
-            <span>{t("nav-mypage")}</span>
-            {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
-          </DropdownMenuItem>
+  if (sessionStatus === "loading") {
+    return <LoginButtonLoadingSkeleton />
+  }
 
-          {role === "admin" && (
-            <>
-              <DropdownMenuSeparator />
-              <DropdownMenuLabel>
-                <div>Admin</div>
-                {/* <span className="text-xs font-normal">{email}</span> */}
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="cursor-pointer"
-                onClick={() => {
-                  router.push("/admin/list")
-                }}
-              >
-                <FileTextIcon className="mr-2 h-4 w-4" />
-                <span>Sales Report</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                className="cursor-pointer"
-                onClick={() => {
-                  router.push("/admin/scanner")
-                }}
-              >
-                <ScanLine className="mr-2 h-4 w-4" />
-                <span>Scanner</span>
-              </DropdownMenuItem>
-            </>
-          )}
-          <DropdownMenuSeparator />
-          <DropdownMenuItem
-            className="cursor-pointer"
-            onClick={() => signOut()}
-          >
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>{t("auth-logout")}</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+  if (sessionStatus === "unauthenticated") {
+    return (
+      <>
+        <Button variant="ghost" size="sm" onClick={() => signIn()}>
+          <LogIn className="mr-2 h-5 w-5" />
+          <span>{t("auth-login")}</span>
+        </Button>
+      </>
     )
   }
+
+  const { mobile, name, role } = session.user
   return (
-    <>
-      <Button variant="ghost" size="sm" onClick={() => signIn()}>
-        <LogIn className="mr-2 h-5 w-5" />
-        <span>{t("auth-login")}</span>
-      </Button>
-    </>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="sm">
+          <User className="h-5 w-5 sm:mr-2" />
+          <span className="hidden sm:inline">{name}</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56" forceMount>
+        <DropdownMenuLabel>
+          <div>My Account</div>
+          <span className="text-xs font-normal text-slate-500">{mobile}</span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onClick={() => {
+            router.push("/user/tickets")
+          }}
+        >
+          <Contact className="mr-2 h-4 w-4" />
+          <span>{t("nav-mypage")}</span>
+          {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
+        </DropdownMenuItem>
+
+        {role === "admin" && (
+          <>
+            <DropdownMenuSeparator />
+            <DropdownMenuLabel>
+              <div>Admin</div>
+              {/* <span className="text-xs font-normal">{email}</span> */}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => {
+                router.push("/admin/list")
+              }}
+            >
+              <FileTextIcon className="mr-2 h-4 w-4" />
+              <span>Sales Report</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => {
+                router.push("/admin/scanner")
+              }}
+            >
+              <ScanLine className="mr-2 h-4 w-4" />
+              <span>Scanner</span>
+            </DropdownMenuItem>
+          </>
+        )}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>{t("auth-logout")}</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
