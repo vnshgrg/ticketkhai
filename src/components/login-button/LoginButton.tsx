2c6e795b733d59d332ded5856e4bbb3331a1782f
Avinash <ghale.avinash@gmail.com>
2024-07-27 11:03:27 +0900

    chore

--- src/components/login-button/LoginButton.tsx
+++ src/components/login-button/LoginButton.tsx
@@ -8,7 +8,6 @@ import {
   User,
 } from "lucide-react"
 import { signIn, signOut, useSession } from "next-auth/react"
-import { useTheme } from "next-themes"
 import useTranslation from "next-translate/useTranslation"
 
 import { Button } from "@/src/components/ui/button"
