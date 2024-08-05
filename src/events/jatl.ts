7fdc6dc6bfa5415d7c8dbb9991e7dcf5e585f0a5
Avinash <ghale.avinash@gmail.com>
2024-08-05 20:10:40 +0900

    add jatl osaka

--- src/events/jatl.ts
+++ src/events/jatl.ts
@@ -93,3 +93,82 @@ export const jatlFukuoka: Event = {
     },
   ],
 }
+
+export const jatlOsaka: Event = {
+  id: "event010",
+  slug: "jatl-osaka",
+  admin: [admins.avinash],
+  title: "John & The Locals",
+  subtitle: "Live in Osaka",
+  description:
+    "John & The Locals for the first time in Japan with their Manai Dekhin 2024 World Tour.",
+  venue: {
+    id: "venue007",
+    title: "Daito Bunka Hall",
+    description: "",
+    address: {
+      id: "address007",
+      postalCode: "574-0037",
+      prefecture: "Osaka",
+      city: "Daito-Shi",
+      addressLine1: "Shinmachi 13-30",
+      mapLink: "https://maps.app.goo.gl/zs97eYPeqYmTYbqR7",
+    },
+    access: "7 minutes walk from Suminodou Station",
+  },
+  dateStart: getTimestamp("2024-09-08 17:00:00"),
+  dateEnd: getTimestamp("2024-09-08 22:00:00"),
+  dateGateOpen: getTimestamp("2024-09-08 15:00:00"),
+  dateTicketSalesStart: getTimestamp("2024-08-20 00:00:00"),
+  maximumNumberOfTicketsAvailable: 1500,
+  photo: "/images/jatl/jatl-osaka.jpg",
+  ogImage: "/images/jatl/jatl-osaka-og.jpg",
+  notices: ["Early birds tickets now on sale!"],
+  confirmPurchase: {
+    title: "John & The Locals Live in Osaka",
+    subtitle: "Osaka",
+    description:
+      "The ticket you are trying to purchase is for OSAKA show. Are you sure you want to continue with your order?",
+  },
+  tickets: [
+    {
+      id: "ticket030",
+      title: "Early birds ticket",
+      description: "Limited discounted price ticket",
+      price: 3500,
+      numberOfTickets: 1,
+      stripePriceId: isProduction
+        ? "price_1PkOlBBE6fqNwbho31u5SwLM"
+        : "price_1PkOdTBE6fqNwbhoTyy0Me3A",
+      available: true,
+      maximumNumberOfTicketsAvailable: 500,
+      dateTicketSalesStart: getTimestamp("2024-08-20 00:00:00"),
+    },
+    {
+      id: "ticket031",
+      title: "Standard ticket",
+      description: "General ticket for single person",
+      price: 4500,
+      numberOfTickets: 1,
+      stripePriceId: isProduction
+        ? "price_1PkOlBBE6fqNwbhobsK75Oqm"
+        : "price_1PkOdTBE6fqNwbhoOqaDwUNn",
+      available: true,
+      maximumNumberOfTicketsAvailable: 500,
+      dateTicketSalesStart: getTimestamp("2024-08-20 00:00:00"),
+    },
+    {
+      id: "ticket032",
+      title: "Couple ticket",
+      description: "Discounted ticket for two people",
+      price: 8000,
+      numberOfTickets: 2,
+      stripePriceId: isProduction
+        ? "price_1PkOlBBE6fqNwbhoYGiU0RyQ"
+        : "price_1PkOdTBE6fqNwbhokY4r7FpH",
+      available: true,
+      maximumNumberOfTicketsAvailable: 300,
+      dateTicketSalesStart: getTimestamp("2024-08-20 00:00:00"),
+    },
+  ],
+}
